27de3d6e3b04ad27a50b4c7155a865d9b4138685
Mr-Ndi <onlythenotes@gmail.com>
2024-08-22 01:34:25 +0200

    part I

--- Node-Blog-Post/app.js
+++ Node-Blog-Post/app.js
@@ -1,2 +1,18 @@
 require('dotenv').config();
 const express = require('express');
+const expressLayout = require('express-ejs-layouts')
+const app = express();
+
+const PORT = 5000 || process.env.PORT
+
+app.use(express.static('public'));
+
+
+app.use(expressLayout)
+app.set('layout', '/layouts/main');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./server/routes/main'));
+app.listen(PORT, () => {
+    console.log('App listern on port ${PORT}');
+});
