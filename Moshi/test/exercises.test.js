c53df7fe0336a4181c315b441b0231d867a9f683
Mr-Ndi <onlythenotes@gmail.com>
2024-12-07 17:30:22 +0200

    FizzBuzz challeng, my answer

---
+++ Moshi/test/exercises.test.js
@@ -0,0 +1,22 @@
+const exer = require('../exercise1')
+
+describe('fizzBuzz', ()=>{
+    it('should throw an error in case non integer input is given',()=>{
+        expect(()=> {fizzBuzz(NaN); }).toThrow();
+    });
+
+    it('should return FizzBuzz for number divisible by 3 and 5',()=>{
+        const answer = exer.fizzBuzz(15)
+        expect(answer).toBe('FizzBuzz')
+    });
+
+    it('should return Buzz for number divisible by 5',()=>{
+        const answer = exer.fizzBuzz(10)
+        expect(answer).toBe('Buzz')
+    });
+
+    it('should return Fizz for number divisible by 3',()=>{
+        const answer = exer.fizzBuzz(12)
+        expect(answer).toBe('Fizz')
+    });
+});
